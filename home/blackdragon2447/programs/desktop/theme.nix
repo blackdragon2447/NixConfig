14fda33f83a9cdfaea88f97e7e7271735e791c32
BlackDragon2447 <blackdragon2447@e.email>
2024-05-09 00:09:15 +0200

    Updoot

--- home/blackdragon2447/programs/desktop/theme.nix
+++ home/blackdragon2447/programs/desktop/theme.nix
@@ -32,7 +32,7 @@ in rec {
 
   qt = {
     enable = true;
-    platformTheme = "gtk";
+    platformTheme.name = "gtk";
     style = {
       name = "gtk2";
       package = pkgs.qt6Packages.qt6gtk2;
