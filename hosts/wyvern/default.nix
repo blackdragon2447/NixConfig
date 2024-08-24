03196f4f0774929acbfdeba1175e77afd511eda8
BlackDragon2447 <blackdragon2447@e.email>
2024-08-24 15:00:42 +0200

    Tlp, save some power/noise when plugged in

--- hosts/wyvern/default.nix
+++ hosts/wyvern/default.nix
@@ -25,6 +25,25 @@
     hostName = "wyvern";
   };
 
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_ENERGY_PERF_POLICY_ON_AC = "balance_power";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+      PLATFORM_PROFILE_ON_AC = "balanced";
+      PLATFORM_PROFILE_ON_BAT = "low-power";
+
+      CPU_BOOST_ON_AC = 0;
+      CPU_BOOST_ON_BAT = 0;
+
+      CPU_HWP_DYN_BOOST_ON_AC = 0;
+      CPU_HWP_DYN_BOOST_ON_BAT = 0;
+
+      RUNTIME_PM_ON_AC = "auto";
+    };
+  };
+
   # tmp hack
 
   boot = {
