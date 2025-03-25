58c0e6f82d4cd3f7d2f7b501db56591a40c7d010
BlackDragon2447 <blackdragon2447@e.email>
2025-03-25 16:56:15 +0100

    Qemu tap

--- hosts/optional/qemu.nix
+++ hosts/optional/qemu.nix
@@ -13,5 +13,62 @@
       systemPackages = with pkgs; [qemu quickemu];
     };
     systemd.tmpfiles.rules = ["L+ /var/lib/qemu/firmware - - - - ${pkgs.qemu}/share/qemu/firmware"];
+
+    systemd.network = {
+      enable = true;
+
+      networks."10-lan" = {
+        matchConfig.Name = ["eno1" "vm-*"];
+        networkConfig = {
+          Bridge = "br0";
+        };
+      };
+
+      netdevs."br0" = {
+        netdevConfig = {
+          Name = "br0";
+          Kind = "bridge";
+        };
+      };
+
+      netdevs."tap0" = {
+        netdevConfig = {
+          Name = "tap0";
+          Kind = "tap";
+        };
+      };
+
+      netdevs."tap1" = {
+        netdevConfig = {
+          Name = "tap1";
+          Kind = "tap";
+        };
+      };
+
+      networks."10-lan-bridge" = {
+        matchConfig.Name = "br0";
+        networkConfig = {
+          Address = ["192.168.0.102/24" "fe80::5322:615b:ff6c:c83c/64"];
+          Gateway = "192.168.0.1";
+          DNS = "192.168.0.1";
+          IPv6AcceptRA = true;
+        };
+        linkConfig.RequiredForOnline = "routable";
+      };
+
+      networks."10-tap0" = {
+        matchConfig.Name = "tap0";
+        networkConfig = {
+          Bridge = "br0";
+        };
+      };
+
+      networks."10-tap1" = {
+        matchConfig.Name = "tap1";
+        networkConfig = {
+          Bridge = "br0";
+        };
+      };
+    };
   };
 }
