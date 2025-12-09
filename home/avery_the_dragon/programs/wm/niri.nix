9bb4e0544519e01bf699fce4cd8374c1489a3af8
BlackDragon2447 <blackdragon2447@e.email>
2025-12-09 11:48:25 +0100

    No more hot corners

--- home/avery_the_dragon/programs/wm/niri.nix
+++ home/avery_the_dragon/programs/wm/niri.nix
@@ -3,7 +3,8 @@
   lib,
   config,
   ...
-}: {
+}:
+{
   options = {
     wm = {
       niri.enable = lib.mkEnableOption "Enable Niri";
@@ -14,22 +15,16 @@
   };
 
   config = lib.mkIf config.wm.niri.enable {
-    home.packages = with pkgs; (
-      [
-        libsForQt5.qtwayland
-        kdePackages.qtwayland
-      ]
-      ++ (
-        if config.wm.niri.audioControls
-        then [pamixer]
-        else []
-      )
-      ++ (
-        if config.wm.niri.playerControls
-        then [playerctl]
-        else []
-      )
-    );
+    home.packages =
+      with pkgs;
+      (
+        [
+          libsForQt5.qtwayland
+          kdePackages.qtwayland
+        ]
+        ++ (if config.wm.niri.audioControls then [ pamixer ] else [ ])
+        ++ (if config.wm.niri.playerControls then [ playerctl ] else [ ])
+      );
 
     # Xwayland?
 
@@ -45,142 +40,138 @@
       exec = "${pkgs.cage}/bin/cage Discord";
       genericName = "All-in-one cross-platform voice and text chat for gamers";
       icon = "discord";
-      mimeType = ["x-scheme-handler/discord"];
+      mimeType = [ "x-scheme-handler/discord" ];
       name = "Discord (Wayland)";
       type = "Application";
     };
 
     xdg.portal.config.niri = {
-      default = ["wlr"];
-      "org.freedesktop.impl.portal.Secret" = ["pass-secret-service"];
+      default = [ "wlr" ];
+      "org.freedesktop.impl.portal.Secret" = [ "pass-secret-service" ];
     };
 
     xdg.portal.config.niri-session = {
-      default = ["wlr"];
-      "org.freedesktop.impl.portal.Secret" = ["pass-secret-service"];
+      default = [ "wlr" ];
+      "org.freedesktop.impl.portal.Secret" = [ "pass-secret-service" ];
     };
 
     programs.niri = {
       enable = true;
       settings = with config.lib.niri.actions; {
-        binds =
-          {
-            "Mod+Shift+Return".action = spawn "${pkgs.kitty}/bin/kitty";
-            # "Mod+Shift+Return".action = spawn "${pkgs.foot}/bin/foot";
-            "Mod+X".action = close-window;
-            "Mod+Shift+Q".action = quit {skip-confirmation = false;};
-            "Mod+E".action = fullscreen-window;
+        binds = {
+          "Mod+Shift+Return".action = spawn "${pkgs.kitty}/bin/kitty";
+          # "Mod+Shift+Return".action = spawn "${pkgs.foot}/bin/foot";
+          "Mod+X".action = close-window;
+          "Mod+Shift+Q".action = quit { skip-confirmation = false; };
+          "Mod+E".action = fullscreen-window;
 
-            "Mod+Return".action = move-column-to-first;
-            "Mod+Shift+Left".action = move-column-left;
-            "Mod+Shift+Right".action = move-column-right;
-            "Mod+Shift+Up".action = move-column-to-workspace-up;
-            "Mod+Shift+Down".action = move-column-to-workspace-down;
+          "Mod+Return".action = move-column-to-first;
+          "Mod+Shift+Left".action = move-column-left;
+          "Mod+Shift+Right".action = move-column-right;
+          "Mod+Shift+Up".action = move-column-to-workspace-up;
+          "Mod+Shift+Down".action = move-column-to-workspace-down;
 
-            "Mod+Ctrl+Left".action = move-column-to-monitor-left;
-            "Mod+Ctrl+Right".action = move-column-to-monitor-right;
-            "Mod+Ctrl+Up".action = move-column-to-monitor-up;
-            "Mod+Ctrl+Down".action = move-column-to-monitor-down;
+          "Mod+Ctrl+Left".action = move-column-to-monitor-left;
+          "Mod+Ctrl+Right".action = move-column-to-monitor-right;
+          "Mod+Ctrl+Up".action = move-column-to-monitor-up;
+          "Mod+Ctrl+Down".action = move-column-to-monitor-down;
 
-            "Mod+Left".action = focus-column-left;
-            "Mod+Right".action = focus-column-right;
-            "Mod+Up".action = focus-window-or-workspace-up;
-            "Mod+Down".action = focus-window-or-workspace-down;
+          "Mod+Left".action = focus-column-left;
+          "Mod+Right".action = focus-column-right;
+          "Mod+Up".action = focus-window-or-workspace-up;
+          "Mod+Down".action = focus-window-or-workspace-down;
 
-            "Mod+Tab".action = switch-preset-column-width;
-            "Mod+Grave".action = maximize-column;
+          "Mod+Tab".action = switch-preset-column-width;
+          "Mod+Grave".action = maximize-column;
 
-            "Mod+Shift+Comma".action = consume-window-into-column;
-            "Mod+Shift+Period".action = expel-window-from-column;
+          "Mod+Shift+Comma".action = consume-window-into-column;
+          "Mod+Shift+Period".action = expel-window-from-column;
 
-            "Mod+Shift+S".action.screenshot = [];
+          "Mod+Shift+S".action.screenshot = [ ];
 
-            "Mod+Shift+Slash".action = show-hotkey-overlay;
-            "Mod+O".action = toggle-overview;
-          }
-          // (lib.optionalAttrs config.wm.niri.audioControls {
-            "XF86AudioMedia" = {
-              action = spawn "playerctl" "play-pause";
-              allow-when-locked = true;
-            };
-            "XF86AudioPlay" = {
-              action = spawn "playerctl" "play-pause";
-              allow-when-locked = true;
-            };
-            "XF86AudioNext" = {
-              action = spawn "playerctl" "next";
-              allow-when-locked = true;
-            };
-            "XF86AudioPrev" = {
-              action = spawn "playerctl" "previous";
-              allow-when-locked = true;
-            };
-          })
-          // (lib.optionalAttrs config.wm.niri.audioControls {
-            "XF86AudioRaiseVolume" = {
-              action = spawn "pamixer" "-i" "5";
-              allow-when-locked = true;
-            };
-            "XF86AudioLowerVolume" = {
-              action = spawn "pamixer" "-d" "5";
-              allow-when-locked = true;
-            };
-            "XF86AudioMute" = {
-              action = spawn "pamixer" "--toggle-mute";
-              allow-when-locked = true;
-            };
-          })
-          // (lib.optionalAttrs config.wm.niri.brightnessControls {
-            "XF86MonBrightnessUp" = {
-              action = spawn "light" "-A" "5";
-              allow-when-locked = true;
-            };
-            "XF86MonBrightnessDown" = {
-              action = spawn "light" "-U" "5";
-              allow-when-locked = true;
-            };
-          })
-          // (lib.optionalAttrs config.desktop-shell.menu.enable {
-            "Mod+P".action = spawn "menu_menu";
-          })
-          // (lib.optionalAttrs config.desktop-shell.swaylock.enable {
-            "Mod+Control+L".action = spawn "swaylock";
-          });
+          "Mod+Shift+Slash".action = show-hotkey-overlay;
+          "Mod+O".action = toggle-overview;
+        }
+        // (lib.optionalAttrs config.wm.niri.audioControls {
+          "XF86AudioMedia" = {
+            action = spawn "playerctl" "play-pause";
+            allow-when-locked = true;
+          };
+          "XF86AudioPlay" = {
+            action = spawn "playerctl" "play-pause";
+            allow-when-locked = true;
+          };
+          "XF86AudioNext" = {
+            action = spawn "playerctl" "next";
+            allow-when-locked = true;
+          };
+          "XF86AudioPrev" = {
+            action = spawn "playerctl" "previous";
+            allow-when-locked = true;
+          };
+        })
+        // (lib.optionalAttrs config.wm.niri.audioControls {
+          "XF86AudioRaiseVolume" = {
+            action = spawn "pamixer" "-i" "5";
+            allow-when-locked = true;
+          };
+          "XF86AudioLowerVolume" = {
+            action = spawn "pamixer" "-d" "5";
+            allow-when-locked = true;
+          };
+          "XF86AudioMute" = {
+            action = spawn "pamixer" "--toggle-mute";
+            allow-when-locked = true;
+          };
+        })
+        // (lib.optionalAttrs config.wm.niri.brightnessControls {
+          "XF86MonBrightnessUp" = {
+            action = spawn "light" "-A" "5";
+            allow-when-locked = true;
+          };
+          "XF86MonBrightnessDown" = {
+            action = spawn "light" "-U" "5";
+            allow-when-locked = true;
+          };
+        })
+        // (lib.optionalAttrs config.desktop-shell.menu.enable {
+          "Mod+P".action = spawn "menu_menu";
+        })
+        // (lib.optionalAttrs config.desktop-shell.swaylock.enable {
+          "Mod+Control+L".action = spawn "swaylock";
+        });
         screenshot-path = "~/Pictures/Screenshots/screenshot_%Y-%m-%d_%H-%M-%S.png";
         prefer-no-csd = true;
-        spawn-at-startup =
-          [
-            {command = ["wpaperd"];}
-            {
-              command = [
-                "${pkgs.cage}/bin/cage"
-                "Discord"
-              ];
-            }
-            {
-              command = [
-                "${pkgs.xwayland-satellite}/bin/xwayland-satellite"
-                ":37"
-              ];
-            }
-            {command = ["dino"];}
-            {command = ["librewolf"];}
-            {command = ["thunderbird"];}
-            {
-              command = [
-                "kitty"
-                "--"
-                "nvim"
-                "-S"
-                "${config.neovim.sessions.neorg-session}"
-              ];
-            }
-          ]
-          ++ (
-            if config.desktop-shell.waybar.enable
-            then [{command = ["waybar"];}]
-            else []
-          );
+        spawn-at-startup = [
+          { command = [ "wpaperd" ]; }
+          {
+            command = [
+              "${pkgs.cage}/bin/cage"
+              "Discord"
+            ];
+          }
+          {
+            command = [
+              "${pkgs.xwayland-satellite}/bin/xwayland-satellite"
+              ":37"
+            ];
+          }
+          { command = [ "dino" ]; }
+          { command = [ "librewolf" ]; }
+          { command = [ "thunderbird" ]; }
+          {
+            command = [
+              "kitty"
+              "--"
+              "nvim"
+              "-S"
+              "${config.neovim.sessions.neorg-session}"
+            ];
+          }
+        ]
+        ++ (if config.desktop-shell.waybar.enable then [ { command = [ "waybar" ]; } ] else [ ]);
+
+        gestures.hot-corners.enable = false;
 
         environment = {
           DISPLAY = ":37";
@@ -234,26 +225,26 @@
 
         window-rules = [
           {
-            matches = [{app-id = "firefox";}];
+            matches = [ { app-id = "firefox"; } ];
             open-on-workspace = "󰈹 ";
           }
 
           {
             matches = [
-              {title = ".*Discord";}
-              {app-id = "cinny";}
-              {app-id = ".*nheko.*";}
+              { title = ".*Discord"; }
+              { app-id = "cinny"; }
+              { app-id = ".*nheko.*"; }
             ];
             open-on-workspace = " ";
           }
 
           {
-            matches = [{app-id = "thunderbird";}];
+            matches = [ { app-id = "thunderbird"; } ];
             open-on-workspace = " ";
           }
 
           {
-            matches = [{app-id = "kitty";}];
+            matches = [ { app-id = "kitty"; } ];
             opacity = 0.9;
           }
         ];
